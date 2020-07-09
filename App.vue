0254e0da903d9f954c04b1518c2083d9bb6bfc44
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2020-07-09 23:13:27 +0545

    Initial Commit

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+    <view class="container">
+        <status-bar
+                background-color="blue"
+                bar-style="light-content"
+        />
+        <text class="text-color-primary">{{ myInput }}</text>
+
+        <button title="Press Me!" @press="addExclamation"></button>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                myInput: "",
+            };
+        },
+        methods: {
+            addExclamation: function () {
+                this.message += "!";
+            },
+        },
+    };
+</script>
+
+<style>
+    .container {
+        background-color: white;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+    }
+
+    .text-color-primary {
+        padding: 10px;
+        font-size: 16px;
+        color: blue;
+    }
+
+    .p15 {
+        padding: 15;
+    }
+</style>
